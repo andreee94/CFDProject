ba53567e8fdaace4ff6c575cfcf714b8b3f6cb20
andrea <andrea26.4.94@gmail.com>
2018-06-17 17:48:29 +0200

    finally a decent way to update checkcompletion

--- mesh-composite/checkcompletion.py
+++ mesh-composite/checkcompletion.py
@@ -2,6 +2,7 @@ import os
 import datetime
 import time, sys
 import argparse
+import curses
 
 def myround(num, decimals=4, strConversion=True):
     num = round(num, decimals)
@@ -28,6 +29,12 @@ def formatTimeDelta(now):
         return  str(int(minutes)) + " min " + str(int(seconds)) + ' s'
 
 
+def delLastLine(n=1):
+    for _ in range(n):
+        sys.stdout.write("\033[F") #back to previous line
+        sys.stdout.write("\033[K") #clear line
+    
+    
 def formatTime(now):
     return str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
 
@@ -49,6 +56,17 @@ endtime = params.endtime
 
 startdate = datetime.datetime.now()
 
+# start manage cursor
+#stdscr = curses.initscr()
+#curses.echo()
+##curses.nocbreak()
+#curses.noecho()
+#curses.cbreak()
+
+print("")
+print("")
+
+first = True
 while not os.path.isdir(mainfolder + "/" + str(endtime)):
     maxtime = 0
     for x in os.listdir(mainfolder):
@@ -58,19 +76,50 @@ while not os.path.isdir(mainfolder + "/" + str(endtime)):
     if perc != 0:
         now = datetime.datetime.now()
         duration = datetime.timedelta(seconds=((now - startdate).seconds/(perc)*100))
+        missing = duration - (datetime.datetime.now() - startdate)
         endestimation = startdate + duration
         
+        str0 = 'Started at = ' + formatTime(startdate) + '. '
         str1 = myround(perc, decimals=2, strConversion=True) + "% completed. "
-        str2 = " Running time = " + formatTimeDelta(datetime.datetime.now() - startdate) +  ". "
+        str2 = "Running time = " + formatTimeDelta(datetime.datetime.now() - startdate) +  ". "
         str3 = "Total time = " + formatTimeDelta(duration) + ". "
-        str4 = ". End estimation = " + formatTime(endestimation) + ". "
+        str4 = "Missing time= " + formatTimeDelta(missing) + ". "
+        str5 = "Ending at = " + formatTime(endestimation) + ". "
         
-        print(str1 + str2 + str3 + str4, end="\r")
-        #sys.stdout.write("\r" )
+        #stdscr.addstr(0, 0, str1)
+        #stdscr.addstr(1, 0, str2)
+        #stdscr.addstr(2, 0, str3)
+        #stdscr.addstr(3, 0, str4)
+        #stdscr.addstr(4, 0, str5)
+        #stdscr.refresh()
+        
+        if not first:
+            delLastLine(n=6)
+        
+        print(str0)
+        print(str1)
+        print(str2)
+        print(str3)
+        print(str4)
+        print(str5)
+        
+        ## Python 3 no cursor version
+        #sys.stdout.write("\033[F") #back to previous line
+        #sys.stdout.write("\033[K") #clear line
+        #print("\r" + str1 + str2 + str3 + str4 + str5) #, end="\r")
+        ## alternative
+        #print("\r" + str1 + str2 + str3 + str4 + str5, end="\r")
         sys.stdout.flush()
+        first = False
+        ###################################
     
     time.sleep(pause)
     
+# stop manage cursor
+#curses.echo()
+#curses.nocbreak()
+#curses.endwin()
+
 print("Completed")
         
         
